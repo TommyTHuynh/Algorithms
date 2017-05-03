773b3547b688d86a2931a2ad9e4fa0875b55091f
BuildTools <unconfigured@null.spigotmc.org>
2017-05-02 22:17:51 -0500

    AVL

--- Algorithms/AVL.h
+++ Algorithms/AVL.h
@@ -1,21 +1,46 @@
 #pragma once
-#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <assert.h>
+
+using std::cout;
+using std::endl;
+using std::max;
+using std::vector;
 
 template <class T>
 struct AVLNode
 {
-	T data;
-	AVLNode *llink;
-	AVLNode *rlink;
+	AVLNode<T> *llink;
+	AVLNode<T> *rlink;
+	T key;
+	int balance;
 };
 
 template <class T>
 class AVL
 {
 private:
-
+	AVLNode<T> *root;
+	AVLNode<T> *findNode(T item, AVLNode<T> *root);
+	AVLNode *findParent(T item, AVLNode<T> *root);
+	int getHeight(AVLNode<T> *root);
+	int getBalance(AVLNode<T> *root);
+	void preorderHelper(AVLNode<T> *root);
+	void inorderHelper(AVLNode<T> *root);
+	void postorderHelper(AVLNode<T> *root);
+	void leftRotate(AVLNode<T> *root);
+	void rightRotate(AVLNode<T> *root);
 public:
-	AVL();
-	~AVL();
+	AVL<T>();
+	~AVL<T>();
+	bool find(T item);
+	void insert(T item);
+	void remove(T item);
+	void preorder();
+	void inorder();
+	void postorder();
+	void treePrint();
 };
 
